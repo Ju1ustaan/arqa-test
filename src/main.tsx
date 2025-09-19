230a92f67b47e02022a2d37ec3c478b02911d4e5
luijulus <luijulus@gmail.com>
2025-09-19 17:13:36 +0500

    main redirect

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
-import Provider from './provider/provider.tsx'
-import App from './App.tsx'
+import Provider from './app/provider/provider.tsx'
+import App from './app/App.tsx'
 import './styles/index.css'
 
 createRoot(document.getElementById('root')!).render(
